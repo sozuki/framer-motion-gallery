5a0fc18e1cf9fca0e20a6073a28e976db92a733a
unknown <nhlsviatoslav@gmail.com>
2024-04-22 14:49:55 +0200

    fixed empty background on transform page

--- app/gallery/(examples)/(curved)/curved-menu/page.tsx
+++ app/gallery/(examples)/(curved)/curved-menu/page.tsx
@@ -1,9 +1,7 @@
-import {CurvedMenu} from "@/ui/menu/curved-menu/curved-menu";
-
 export default function Page() {
   return (
     <div className="w-full h-screen flex justify-end bg-white ">
-      <CurvedMenu/>
+
     </div>
   )
 }
